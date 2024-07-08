2da5dc3d1ff69c2fbe8449927b7c273f03826e8e
Mazikooo <153087981+Mazikooo@users.noreply.github.com>
2024-07-08 07:58:02 +0700

    Update midtrans.php

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'merchant_id' => env('MIDTRANS_MERCHANT_ID', 'G697141332'),
-    'client_key' => env('MIDTRANS_CLIENT_KEY', 'SB-Mid-client-o0XeRQiRZfcZzWT5'),
-    'server_key' => env('MIDTRANS_SERVER_KEY', 'SB-Mid-server-GqjL8nGQIhV1I5TZwWpUhwEq'),
+    'merchant_id' => env('MIDTRANS_MERCHANT_ID', ''),
+    'client_key' => env('MIDTRANS_CLIENT_KEY', 'SB-Mid-client-'),
+    'server_key' => env('MIDTRANS_SERVER_KEY', 'SB-Mid-server-'),
     'is_production' => false, // Set to true if you want to use the production environment
     'is_sanitized' => env('MIDTRANS_IS_SANITIZED', true),
     'is_3ds' => env('MIDTRANS_IS_3DS', true),
